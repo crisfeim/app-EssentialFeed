1ecb4940ed45cca0edc89c2c7e75e01c9ddd95f0
Cristian Rojas <cristian.rojas@live.fr>
2024-11-28 00:35:29 +0100

    Move mapping logic to a new type `FeedItemsMapper`

--- EssentialFeed/EssentialFeed/FeedApi/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedApi/RemoteFeedLoader.swift
@@ -40,8 +40,8 @@ public final class RemoteFeedLoader {
             
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items.map(\.item)))
+                if let items = try? FeedItemsMapper.map(data, response) {
+                    completion(.success(items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -52,21 +52,33 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+private enum FeedItemsMapper {
     
-    var item: FeedItem {
-        FeedItem(
-            id: id,
-            description: description,
-            location: location,
-            imageURL: image)
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            FeedItem(
+                id: id,
+                description: description,
+                location: location,
+                imageURL: image)
+        }
+    }
+    
+    private static let jsonDecoder = JSONDecoder()
+    
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        return try jsonDecoder.decode(Root.self, from: data).items.map(\.item)
     }
 }
