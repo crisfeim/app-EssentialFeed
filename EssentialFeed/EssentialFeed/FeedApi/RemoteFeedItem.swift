cf6df4ea2b34efa8eb5fcf633a647fe2a47d00f8
Cristian Rojas <cristian.rojas@live.fr>
2025-04-03 13:25:02 +0200

    Move `RemoteFeedItem` to its own file

---
+++ EssentialFeed/EssentialFeed/FeedApi/RemoteFeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Cristian Felipe Patiño Rojas on 3/4/25.
+//
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
